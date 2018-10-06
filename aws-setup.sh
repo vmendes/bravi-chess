11fff57e01a19cd0fbc0a66ae86e543a38b33921
Valdir Mendes <valdir.mendes@gmail.com>
2018-10-06 18:34:17 -0300

    AWS setup

--- aws-setup.sh
+++ aws-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# if you have an AWS instance, run the commans below for immediate installation
+# If you have an AWS instance, run the commans below for immediate installation
 # Note: don't forget to expose Inbound ports on the instance (ports 8888/4200)
 #
 # wget https://raw.githubusercontent.com/vmendes/bravi-chess/master/aws-setup.sh
@@ -11,16 +11,18 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 
 apt-get update
 apt-cache policy docker-ce
-apt-get install -y docker-ce
-systemctl status docker
-apt-get install -y docker-compose
-apt-get install maven
-apt-get install nodejs
-apt-get install npm
+apt-get --assume-yes install -y docker-ce
+apt-get --assume-yes install -y docker-compose
+apt-get --assume-yes install maven
+apt-get --assume-yes install nodejs
+apt-get --assume-yes install npm
 
 git clone https://github.com/vmendes/bravi-chess.git
 cd bravi-chess/bravi-chess-api
 mvn package
 cd ..
 
-#docker-compose up --build
+# After all installed
+#
+# cd bravi-chess
+# sudo docker-compose up --build
